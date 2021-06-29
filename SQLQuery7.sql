2cba84a3a1c8895098fe65eb70b537cf011b1931
Shubhasree Sarkar <71165596+shubhasree1234@users.noreply.github.com>
2021-06-29 23:01:02 +0530

    Update SQLQuery7.sql

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,11 +1,12 @@
-USE IVY_practise;
+Problem Statement- Use the given data to find the customer with the highest total order cost within a single day  between 2021-02-01 to 2021-03-01.
+Total order cost is calculated as order_cost*order_quantity. Output their first name, total sales amount and the date. 
+For simplicity, you can assume that every first name in the dataset is unique. Also, the cost of the certain item 
+(e.g. shoes) could vary among different purchases (not all shoes cost the same)
 
-#Use the given data to find the customer with the highest total order cost within a single day  between 
-#2021-02-01 to 2021-03-01.
-#Total order cost is calculated as order_cost*order_quantity. 
-#Output their first name, total sales amount and the date. 
-#For simplicity, you can assume that every first name in the dataset is unique. Also, the cost of the certain item 
-#(e.g. shoes) could vary among different purchases (not all shoes cost the same)
+Table- Customers,Orders
+IDE- MS SQL Server
+
+USE IVY_practise;
 
 SELECT * FROM (
 SELECT c.firstname ,(o.Order_cost*o.order_quantity) AS Highest_Total_Sales, CAST(o.order_date AS DATE) Order_date,
